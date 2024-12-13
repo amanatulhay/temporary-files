2550de7105ed84c00b347ceb255944b4bd57c764
amanatulhay <amanatulhay.pribadi@gmail.com>
2024-12-13 20:02:07 +0800

    mongo

---
+++ reguler-day9/src/models/category.model.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    products: [{
+      type: Schema.Types.ObjectId,
+      ref: "Products",
+    }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const CategoryModel = mongoose.model("Category", CategorySchema);
+
+export default CategoryModel;
